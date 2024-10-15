83a0ca42519f5215fae94ad847943b45137a0f29
Ivan <ivanglzr01@gmail.com>
2024-10-15 22:32:51 +0200

    add car schema to imports

--- src/car/car.module.ts
+++ src/car/car.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { CarController } from './car.controller';
 import { CarService } from './car.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Car, CarSchema } from './schemas/car.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Car.name, schema: CarSchema }])],
   controllers: [CarController],
   providers: [CarService],
 })
