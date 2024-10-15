b0af280901ed38cc5d1fb26ba24661229228011c
Ivan <ivanglzr01@gmail.com>
2024-10-15 22:32:06 +0200

    update car service

--- src/car/car.service.ts
+++ src/car/car.service.ts
@@ -1,219 +1,37 @@
 import { Injectable } from '@nestjs/common';
 
-import { FuelTypes, type Car } from './interfaces/car.interface';
 import type { SearchCar } from './interfaces/search-car.interface';
-import { CreateCarDto } from './dto/create-car.dto';
+import type { CreateCarDto } from './dto/create-car.dto';
+import type { Model } from 'mongoose';
+import type { UpdateCarDto } from './dto/update-car.dto';
+
+import { InjectModel } from '@nestjs/mongoose';
+
+import { Car } from './schemas/car.schema';
 
 @Injectable()
 export class CarService {
-  private cars: Car[] = [
-    {
-      id: 1,
-      brand: 'Toyota',
-      model: 'Corolla',
-      year: 2020,
-      color: 'blue',
-      isElectric: false,
-      fuelType: FuelTypes.gasoline,
-    },
-    {
-      id: 2,
-      brand: 'Tesla',
-      model: 'Model 3',
-      year: 2021,
-      isElectric: true,
-      fuelType: FuelTypes.electric,
-    },
-    {
-      id: 3,
-      brand: 'Ford',
-      model: 'F-150',
-      year: 2019,
-      color: 'red',
-      isElectric: false,
-      fuelType: FuelTypes.gasoline,
-    },
-    {
-      id: 4,
-      brand: 'Nissan',
-      model: 'Leaf',
-      year: 2022,
-      isElectric: true,
-      fuelType: FuelTypes.electric,
-    },
-    {
-      id: 5,
-      brand: 'Chevrolet',
-      model: 'Bolt',
-      year: 2021,
-      color: 'yellow',
-      isElectric: true,
-      fuelType: FuelTypes.electric,
-    },
-    {
-      id: 6,
-      brand: 'Honda',
-      model: 'Civic',
-      year: 2020,
-      color: 'black',
-      isElectric: false,
-      fuelType: FuelTypes.gasoline,
-    },
-    {
-      id: 7,
-      brand: 'BMW',
-      model: 'X3',
-      year: 2021,
-      isElectric: false,
-      fuelType: FuelTypes.diesel,
-    },
-    {
-      id: 8,
-      brand: 'Audi',
-      model: 'A4',
-      year: 2022,
-      color: 'white',
-      isElectric: false,
-      fuelType: FuelTypes.gasoline,
-    },
-    {
-      id: 9,
-      brand: 'Hyundai',
-      model: 'Kona',
-      year: 2021,
-      color: 'green',
-      isElectric: true,
-      fuelType: FuelTypes.electric,
-    },
-    {
-      id: 10,
-      brand: 'Volkswagen',
-      model: 'ID.4',
-      year: 2022,
-      isElectric: true,
-      fuelType: FuelTypes.electric,
-    },
-    {
-      id: 11,
-      brand: 'Kia',
-      model: 'Niro',
-      year: 2020,
-      color: 'blue',
-      isElectric: true,
-      fuelType: FuelTypes.electric,
-    },
-    {
-      id: 12,
-      brand: 'Mazda',
-      model: 'CX-5',
-      year: 2020,
-      color: 'red',
-      isElectric: false,
-      fuelType: FuelTypes.gasoline,
-    },
-    {
-      id: 13,
-      brand: 'Subaru',
-      model: 'Outback',
-      year: 2021,
-      isElectric: false,
-      fuelType: FuelTypes.gasoline,
-    },
-    {
-      id: 14,
-      brand: 'Porsche',
-      model: 'Taycan',
-      year: 2022,
-      isElectric: true,
-      fuelType: FuelTypes.electric,
-    },
-    {
-      id: 15,
-      brand: 'Mercedes-Benz',
-      model: 'C-Class',
-      year: 2020,
-      color: 'silver',
-      isElectric: false,
-      fuelType: FuelTypes.gasoline,
-    },
-    {
-      id: 16,
-      brand: 'Volvo',
-      model: 'XC40',
-      year: 2021,
-      color: 'black',
-      isElectric: false,
-      fuelType: FuelTypes.diesel,
-    },
-    {
-      id: 17,
-      brand: 'Jaguar',
-      model: 'I-PACE',
-      year: 2022,
-      isElectric: true,
-      fuelType: FuelTypes.electric,
-    },
-    {
-      id: 18,
-      brand: 'Chrysler',
-      model: 'Pacifica',
-      year: 2021,
-      isElectric: false,
-      fuelType: FuelTypes.gasoline,
-    },
-    {
-      id: 19,
-      brand: 'Land Rover',
-      model: 'Range Rover',
-      year: 2020,
-      color: 'green',
-      isElectric: false,
-      fuelType: FuelTypes.diesel,
-    },
-    {
-      id: 20,
-      brand: 'Lincoln',
-      model: 'Navigator',
-      year: 2022,
-      isElectric: false,
-      fuelType: FuelTypes.gasoline,
-    },
-    {
-      id: 21,
-      brand: 'Buick',
-      model: 'Enclave',
-      year: 2021,
-      color: 'purple',
-      isElectric: false,
-      fuelType: FuelTypes.gasoline,
-    },
-  ];
+  constructor(@InjectModel(Car.name) private carModel: Model<Car>) {}
 
-  getCars(query: SearchCar): Car[] {
-    return this.filterCars(query);
+  async getCars(query: SearchCar): Promise<Car[]> {
+    return await this.carModel.find({ ...query });
   }
 
-  getCar(id: number): Car {
-    return this.cars.find((car) => car.id === id);
+  async getCar(id: string): Promise<Car> {
+    return await this.carModel.findById(id);
   }
 
-  postCar(car: CreateCarDto) {
-    const newCar = {
-      ...car,
-      id: this.cars.length + 1,
-    } as Car;
+  async postCar(car: CreateCarDto) {
+    const newCar = new this.carModel(car);
 
-    this.cars.push(newCar);
+    await newCar.save();
+
+    return newCar;
   }
 
-  private filterCars(query: SearchCar): Car[] {
-    return this.cars.filter((car) => {
-      return Object.entries(query).every(([key, value]) => {
-        return (
-          car[key as keyof Car]?.toString().toLowerCase() ===
-          value.toString().toLowerCase()
-        );
-      });
-    });
+  async putCar(id: string, car: UpdateCarDto) {
+    const updatedCar = await this.carModel.findByIdAndUpdate(id, car);
+
+    return updatedCar;
   }
 }
